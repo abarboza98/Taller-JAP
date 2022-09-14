c3b733e9686e8b61e2de6b0ba51484733f56df7c
Agustin Barboza <theiiagus@gmail.com>
2022-09-14 00:42:40 -0300

    Añado Carousel e imagenes ilustrativas como Carousel

--- js/product-info.js
+++ js/product-info.js
@@ -1,7 +1,7 @@
 let productID = localStorage.getItem('productID');
 let INFO_PRODUCT = PRODUCT_INFO_URL + productID + EXT_TYPE;
 let PRODUCT_COMMENTS = PRODUCT_INFO_COMMENTS_URL + productID + EXT_TYPE;
-
+let currentCommentsList = [];
 //FUNCION QUE MUESTRA NOMBRE, PRECIO, DESCRIPCION, CANTIDAD DE VENDIDOS E IMAGENES DE CADA PRODUCTO
 
 function showInfo(dataInfo) {
@@ -10,11 +10,11 @@ function showInfo(dataInfo) {
 <div class="p-4 columns">
 
   <div class="mb-3">
-  <h2 class="font-weight-bold">${dataInfo.name}</h2>
-  <hr>
-  <h4 class="font-weight-bold">Precio</h4>
-  <p>${dataInfo.currency + dataInfo.cost}
-  <br>
+   <h2 class="font-weight-bold">${dataInfo.name}</h2>
+    <hr>
+      <h4 class="font-weight-bold">Precio</h4>
+   <p>${dataInfo.currency + dataInfo.cost}
+    <br>
   <h4 class="font-weight-bold">Descripcion</h4>
 
   <p>${dataInfo.description}</p>
@@ -37,13 +37,40 @@ function showInfo(dataInfo) {
   
 </div>
   `;
+  // AÑADE IMAGENES ILUSTRATIVAS
+  let appendImage = `
+  
+    <div class="carousel slide" data-mdb-ride="carousel">
+    <div id="carouselExampleControls" class="carousel slide container" data-bs-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="${dataInfo.images[0]}" class="d-block w-100">
+                </div>
+                <div class="carousel-item">
+                    <img src="${dataInfo.images[1]}" class="d-block w-100">
+                </div>
+                <div class="carousel-item">
+                    <img src="${dataInfo.images[2]}" class="d-block w-100">
+                </div>
+                <div class="carousel-item">
+                    <img src="${dataInfo.images[3]}" class="d-block w-100">
+                </div>
+              
+                <button style="color:black;" class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+                </button>
+                         
+            </div>
+        </div>
+  </div>
+          `;
 
-  for (let imagen = 0; imagen < dataInfo.images.length; imagen++) {
-    htmlContentToAppend += `
-    <div class="d-flex w-10 justify-content-center row" >
-      <img src="${dataInfo.images[imagen]}" class="img-thumbnail img-responsive col-sm-4">
-    </div>`;
-  }
+  document.getElementById('imgCarousel').innerHTML = appendImage;
   document.getElementById('producto').innerHTML = htmlContentToAppend;
 }
 
@@ -51,17 +78,29 @@ function showInfo(dataInfo) {
 function showComments(dataComments) {
   arrayComments = dataComments;
   let appendCommentsToHTML = '';
+
   for (let comentario = 0; comentario < arrayComments.length; comentario++) {
+    //Muestro la puntuacion con estrellas
+    let estrellas = '';
+    for (let i = 1; i <= 5; i++) {
+      if (i <= arrayComments[comentario].score) {
+        estrellas += '<i class="fas fa-star checked" ></i>'; //icono estrella llena
+      } else {
+        estrellas += '<i class="far fa-star checked"></i>'; //icono contorno estrella
+      }
+    }
+
     appendCommentsToHTML += `
       <div class="list-group-item container d-flex w-100 " >
             <span ><h3 class='font-weight-bold'>${arrayComments[comentario].user}</h3>           
-              <p>${arrayComments[comentario].dateTime}</p>
+              <p>${arrayComments[comentario].dateTime}</p><span>${estrellas}</span>
                 <br>
               <p>${arrayComments[comentario].description}</p>
             </span>
       </div>`;
+
+    document.getElementById('comentarios').innerHTML = appendCommentsToHTML;
   }
-  document.getElementById('comentarios').innerHTML += appendCommentsToHTML;
 }
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -75,8 +114,37 @@ document.addEventListener('DOMContentLoaded', () => {
   //PETICIÓN DE LA URL PARA MOSTRAR LOS COMENTARIOS DE CADA PRODUCTO
   getJSONData(PRODUCT_COMMENTS).then(function (resultObj) {
     if (resultObj.status === 'ok') {
-      let currentCommentsProduct = resultObj.data;
-      showComments(currentCommentsProduct);
+      currentCommentsList = resultObj.data;
+      showComments(currentCommentsList);
     }
   });
+});
+
+//ENVIA LOS COMENTARIOS
+document.getElementById('sendComment').addEventListener('click', () => {
+  //obtiene la fecha actual
+  const actualDate = new Date();
+  const year = actualDate.getFullYear();
+  const month = actualDate.getMonth() + 1;
+  const day = actualDate.getDate();
+  const hour = actualDate.getHours();
+  const minute = actualDate.getMinutes();
+  const second = actualDate.getSeconds();
+
+  let myComment = {
+    product: localStorage.getItem('productID'),
+    score: document.getElementById('puntaje').value,
+    description: document.getElementById('descripcion').value,
+    user: localStorage.getItem('user'),
+    dateTime: `${year}-${month}-${day} ${hour}:${minute}:${second}`,
+  };
+  currentCommentsList.push(myComment);
+
+  Swal.fire({
+    icon: 'success',
+    text: 'Comentario enviado con exito!',
+  });
+  showComments(currentCommentsList);
+  document.getElementById('description').value = '';
+  document.getElementById('puntaje').value = 3;
 });
