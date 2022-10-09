ba7c9a43055b75bd96488120c98977f28fe31682
theiiagus <theiiagus@gmail.com>
2022-10-09 12:10:32 -0300

    Añado funcionalidad de calcular precios por cantidad

--- js/cart.js
+++ js/cart.js
@@ -1,5 +1,18 @@
 let CART_URL = CART_INFO_URL + 25801 + EXT_TYPE;
 
+function totalPrice() {
+  let cantidad =
+    document.getElementsByClassName('quantity')[
+      document.getElementsByClassName('quantity').length - 1
+    ].value;
+  let actualArticle = currentCartArticles[currentCartArticles.length - 1];
+  let valorTotal = cantidad * actualArticle.unitCost;
+
+  document.getElementById(
+    'valorTotal'
+  ).innerHTML = `${actualArticle.currency} ${valorTotal}`;
+}
+
 function showCart(articleCart) {
   let htmlContentToAppend = '';
   for (let nroArticulo = 0; nroArticulo < articleCart.length; nroArticulo++) {
@@ -23,39 +36,39 @@ function showCart(articleCart) {
     
                             <div class="d-flex align-items-center">
                               <p class="fw-bold mb-0 me-5 pe-3">${articulo.currency}${articulo.unitCost}</p>
-                              <div
-                                class="def-number-input number-input safari_only"
-                              >
-                                <button
-                                  onclick="this.parentNode.querySelector('input[type=number]').stepDown()"
-                                  class="minus"
-                                ></button>
-                                <input
-                                  class="quantity fw-bold text-black"
-                                  min="0"
-                                  name="quantity"
-                                  value="1"
-                                  type="number"
-                                />
-                                <button
-                                  onclick="this.parentNode.querySelector('input[type=number]').stepUp()"
-                                  class="plus"
-                                ></button>
-                              </div>
+                              <div class="d-flex flex-row">
+                              <button class="btn btn-link px-2"
+                                onclick="this.parentNode.querySelector('input[type=number]').stepDown(), totalPrice()">
+                                <i class="fas fa-minus"></i>
+                              </button>
+          
+                              <input id="form1" min="0" name="quantity" value="1" type="number"
+                                class="form-control form-control-sm quantity" style="width: 50px;" 
+                                onchange="totalPrice()" />
+          
+                              <button class="btn btn-link px-2"
+                                onclick="this.parentNode.querySelector('input[type=number]').stepUp(), totalPrice()"
+                                 >
+                                <i class="fas fa-plus"></i>
+                                
+                              </button>
+                            </div>
                             </div>
                           </div>
       </div>
       
       `;
+    document.getElementById('valorTotal').innerHTML += articulo.unitCost;
   }
+
   document.getElementById('agregarArticulo').innerHTML += htmlContentToAppend;
 }
 
 document.addEventListener('DOMContentLoaded', () => {
   getJSONData(CART_URL).then(function (resultObj) {
     if (resultObj.status === 'ok') {
-      let currentCartArticles = resultObj.data.articles;
-      console.log(currentCartArticles[0].image);
+      currentCartArticles = resultObj.data.articles;
+
       showCart(currentCartArticles);
     }
   });
