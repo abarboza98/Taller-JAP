0bbda9840ec32222a7925972f2a1d3170309524c
theiiagus <theiiagus@gmail.com>
2022-11-13 21:08:22 -0300

    New Feature: Añadiendo imagen de usuario

--- js/my-profile.js
+++ js/my-profile.js
@@ -21,12 +21,27 @@ function verifiedProfile() {
 }
 
 function editProfile() {
+  let imgSelected = imagenUser.files;
   localStorage.setItem('pNombre', primerNombre.value);
   localStorage.setItem('sNombre', segundoNombre.value);
   localStorage.setItem('pApellido', primerApellido.value);
   localStorage.setItem('sApellido', segundoApellido.value);
   localStorage.setItem('email', email.value);
   localStorage.setItem('nroContacto', numeroContacto.value);
+
+  if (imgSelected.length > 0) {
+    let imgToB64 = imgSelected[0];
+    let fileReader = new FileReader();
+
+    fileReader.onload = function (event) {
+      let b64Data = event.target.result;
+
+      document.getElementById('img-perfil').src = b64Data;
+      localStorage.setItem('imgUser', b64Data);
+    };
+
+    fileReader.readAsDataURL(imgToB64);
+  }
 
   if (usuario.value != '' && usuario.value != localStorage.getItem('user')) {
     localStorage.setItem('user', usuario.value);
@@ -49,27 +64,18 @@ function getProfile() {
     localStorage.getItem('nroContacto');
 
   document.getElementById('nameUser').innerHTML = localStorage.getItem('user');
+
+  if (localStorage.getItem('imgUser') === null) {
+    document.getElementById('img-perfil').src = '/img/img_perfil.png';
+  } else {
+    document.getElementById('img-perfil').src = localStorage.getItem('imgUser');
+  }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
   getProfile();
 
-  imagenUser.addEventListener('change', () => {
-    let imgSelected = imagenUser.files;
-
-    if (imgSelected.length > 0) {
-      let imgToB64 = imgSelected[0];
-      let fileReader = new FileReader();
-
-      fileReader.onload = function (event) {
-        let b64Data = event.target.result;
-
-        document.getElementById('img-perfil').src = b64Data;
-      };
-
-      fileReader.readAsDataURL(imgToB64);
-    }
-  });
+  imagenUser.addEventListener('change', () => {});
 
   document
     .getElementById('formProfile')
@@ -91,8 +97,6 @@ document.addEventListener('DOMContentLoaded', () => {
         icon: 'success',
         timer: 2500,
         showConfirmButton: false,
-      }).then(() => {
-        window.location = 'my-profile.html';
       });
     }
   });
